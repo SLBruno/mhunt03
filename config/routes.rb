3164b279599a8a761e13fbd14212afb13c0732f2
brunosl <brunolagoela@gmail.com>
2015-11-15 20:23:22 -0200

    v0.57 - adicionado página de usuário

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
     devise_for :users
+    resources :users, only: :show
     resources :comments
     resources :links
     resources :videos, only: [:index, :new, :create, :edit, :show] do
